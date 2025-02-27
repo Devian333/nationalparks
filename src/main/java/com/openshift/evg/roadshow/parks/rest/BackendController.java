fc166c9f03c9ba1a0ed32f8ee085cb585d25fc4a
Devian333 <andrey.beliaev@hotmail.com>
2025-02-27 14:01:55 +0100

    Update BackendController.java

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public Backend get() {
-        return new Backend("nationalparks","Super Awesome National Parks (Commit #4, after latest change)", new Coordinates("47.039304", "14.505178"), 4);
+        return new Backend("nationalparks","Super Awesome National Parks (Commit #5..)", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
